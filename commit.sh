0737fba2b5e32c374cac25474bf21e870c1d603d
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 14:09:36 +0800

    test commit

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-var_comd=$(git add . && git commit -m 'test commit' && git push)
-
+git add . && git commit -m 'test commit'
+var_comd=$(git push)
 echo "==========================================================="
 echo $var_comd
 if [[ $var_comd == *"remote: Resolving deltas: 100%"* ]]
