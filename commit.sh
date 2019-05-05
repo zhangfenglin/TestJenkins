9bf53073dea635e8288d5861af28b711b0658900
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 12:07:07 +0800

    test commit

--- commit.sh
+++ commit.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 var_comd=$(git add . && git commit -m 'test commit')
-echo '=============================================='
-echo $var_comd
 if [[ $var_comd == *changed,* ]]
 then
     echo "commit success ************"
