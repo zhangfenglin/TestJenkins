c72e0357f1a8fd52c7ba9bd185ec77f8b85bf9b2
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 12:29:27 +0800

    test commit

--- commit.sh
+++ commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 var_comd=$(git add . && git commit -m 'test commit' && git push)
-echo $var_comd | grep -E "Your branch is ahead of 'origin/*' by * commits."
+data=$(echo $var_comd | grep -E "Your branch is ahead of 'origin/*' by * commits.")
+echo '============================================'
+echo $data
 
 
 #if [[ $var_comd == *changed,* ]]
