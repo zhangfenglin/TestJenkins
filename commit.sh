6bdc3f011a1843c8ebb9b225d102537989d8ee49
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 12:06:52 +0800

    test commit

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@
 var_comd=$(git add . && git commit -m 'test commit')
 echo '=============================================='
 echo $var_comd
-for line in $var_comd; do
-    echo $line
-done
+if [[ $var_comd == *changed,* ]]
+then
+    echo "commit success ************"
+fi
