c9c9c9d361d1ec134e8ce35f2a1b83940746f371
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 12:24:23 +0800

    test commit

--- commit.sh
+++ commit.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 var_comd=$(git add . && git commit -m 'test commit' && git push)
-echo $var_comd
+echo $var_comd | grep -E "Your branch is ahead of 'origin/*' by * commits."
 
-if [[ $var_comd == *changed,* ]]
-then
-    echo "commit success ************"
-else
-    echo "failed"
-fi
+
+#if [[ $var_comd == *changed,* ]]
+#then
+#    echo "commit success ************"
+#else
+#    echo "failed"
+#fi
