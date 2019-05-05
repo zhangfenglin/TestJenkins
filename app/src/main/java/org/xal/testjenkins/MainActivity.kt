c6fb98ae337b19744c798ed5f0b65ccda58688f1
zhangfenglin <zhangfenglin@apusapps.com>
2019-05-05 11:55:10 +0800

    test commit

--- app/src/main/java/org/xal/testjenkins/MainActivity.kt
+++ app/src/main/java/org/xal/testjenkins/MainActivity.kt
@@ -3,6 +3,7 @@ package org.xal.testjenkins
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// test
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
